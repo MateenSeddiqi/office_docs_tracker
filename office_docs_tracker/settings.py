fb633a5e4f34c27f92a3f9eb5cfc1675ec791fd5
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-01-09 10:13:16 +0430

    new apps added and media and statics file setup

--- office_docs_tracker/settings.py
+++ office_docs_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'records',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[BASE_DIR / "static"] # use this code when the static folder of each app is in one base directory. 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #use this code when every app have their own static folder.  
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
