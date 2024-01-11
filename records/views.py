09315f99896595b22cd8a05e1ec5dab70f15436a
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-01-11 10:13:18 +0430

    Tailwind add and record urls, view added to main project

--- records/views.py
+++ records/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import IncomingDoc, OutgoingDoc
 # Create your views here.
+
+
+def records(request):
+    return render (request, 'records/records.html')
