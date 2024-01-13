79444643d5492f2d66455a56d5fa37f0682092ea
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-01-13 10:24:09 +0430

    adding commits

--- records/models.py
+++ records/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
 
 class IncomingDoc(models.Model):
     continuos_number = models.IntegerField(unique=True)
--- records/views.py
+++ records/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import IncomingDoc, OutgoingDoc
 # Create your views here.
 
-
-def incomingDoc(request):
+# code of incoming docs to display incoming files
+def incomingDoc(request):  
     incomingDocs = IncomingDoc.objects.all()
     return render (request, 'records/incomingDocs.html', {
         'incomingDocs': incomingDocs,
         })
 
+# code of outgoing docs to display outgoing files
 def outgoingDoc(request):
     outgoingDocs = IncomingDoc.objects.all()
     return render (request, 'records/outgoingDocs.html', {
