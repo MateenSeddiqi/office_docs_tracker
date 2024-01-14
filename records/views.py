b4ad51ec2201f03e5ee8cd6f301f5faa47b7ee4d
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-01-14 16:04:44 +0430

    Incoming docs form created and setup

--- records/views.py
+++ records/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import IncomingDoc, OutgoingDoc
 from .forms import IncomingDocForm
+from django.contrib import messages
+
+
 # Create your views here.
 
 def records(request):
@@ -10,17 +13,27 @@ def records(request):
 # code of incoming docs to display incoming files
 def incomingDoc(request):  
     incomingDocs = IncomingDoc.objects.all()
+    print(incomingDocs)
     return render (request, 'records/incomingDocs.html', {
         'incomingDocs': incomingDocs,
         })
 
 
 def addIncomingDoc(request):
-    # if request.method == 'POST':
-    incomingDocForm = IncomingDocForm()
-    return render (request, 'records/incomingDocForm.html', {
-        'incomingDocForm': incomingDocForm
-    }) 
+    incoming_doc_form = IncomingDocForm()
+    if request.method == 'POST':
+        incoming_doc_form = IncomingDocForm(request.POST, request.FILES)
+
+        if incoming_doc_form.is_valid():
+            newdoc = incoming_doc_form.save()
+            return redirect('records:incomingDoc')  #pk=newdoc.id
+            messages.success(request, 'Added')
+        else:
+            messages.error(request, 'Not  Added')
+            
+    context ={'incomingDocForm': incoming_doc_form} 
+    return render(request, 'records/incomingDocForm.html', context)   
+
 
 
 
